e116bc3058d62109417ce6761e7c8e5b7655938c
David Renshaw <dwrenshaw@gmail.com>
2024-06-28 14:32:24 -0400

    lambdaman solver commandline argument

--- lambdaman-solver/src/main.rs
+++ lambdaman-solver/src/main.rs
@@ -1,15 +1,12 @@
 #![allow(dead_code)]
 
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use std::fs;
 use std::path::Path;
 use std::process::{Command, Stdio};
 use std::collections::{BTreeSet, VecDeque};
 
-
 use lambdaman::{Item, Coords, State, Move, parse_lambda_puzzle};
-
-//HashSet<S
 
 #[derive(Debug)]
 struct SearchNode {
@@ -65,8 +62,13 @@ fn main() -> Result<()> {
     let git_dir = String::from_utf8(output.stdout).unwrap();
     let gd = git_dir.trim();
 
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 2 {
+        return Err(anyhow!("usage: {} PUZZLE_NUMBER", args[0]));
+    }
+
     let puzzpath = Path::new(gd)
-        .join("puzzles/lambdaman/lambdaman4.txt")
+        .join(format!("puzzles/lambdaman/lambdaman{}.txt", args[1]))
         .into_os_string()
         .into_string()
         .unwrap();
