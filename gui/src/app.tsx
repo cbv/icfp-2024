ce93cf8776f26de19f059b04904cea64f252544c
Jason Reed <jcreed@gmail.com>
2024-06-28 11:50:57 -0400

    More refactoring of mode state

--- gui/src/app.tsx
+++ gui/src/app.tsx
@@ -4,7 +4,7 @@ import { doEffect } from './effect';
 import { extractEffects } from './lib/extract-effects';
 import { useEffectfulReducer } from './lib/use-effectful-reducer';
 import { reduce } from './reduce';
-import { AppMode, AppState, mkState } from './state';
+import { AppMode, AppModeState, AppState, mkState } from './state';
 import { Dispatch } from './action';
 import { toTitleCase } from './lib/util';
 import { decodeString, encodeString } from './codec';
@@ -28,57 +28,76 @@ export function Navbar(props: { state: AppState, dispatch: Dispatch }): JSX.Elem
 
 }
 
-function renderAppBody(state: AppState, dispatch: Dispatch): JSX.Element {
-
+function renderEvaluate(state: AppState, modeState: AppModeState & { t: 'evaluate' }, dispatch: Dispatch): JSX.Element {
   const onInput: React.FormEventHandler<HTMLTextAreaElement> = (e) => { dispatch({ t: 'setInputText', text: e.currentTarget.value }); };
+  return <div className="interface-container">
+    <div className="textarea-container">
+      <div className="textarea-panel">
+        <div className="title">Input</div>
+        <textarea spellCheck={false} value={state.modeState.inputText} onInput={onInput}></textarea>
+      </div>
+      <div className="textarea-panel">
+        <div className="title">Output</div>
+        <textarea spellCheck={false} value={state.modeState.outputText}></textarea>
+      </div>
+    </div>
+    <div className="action-bar">
+      <button>Evaluate</button>
+    </div>
+  </div>;
+}
 
+function renderCodec(state: AppState, modeState: AppModeState & { t: 'codec' }, dispatch: Dispatch): JSX.Element {
+  const onInput: React.FormEventHandler<HTMLTextAreaElement> = (e) => { dispatch({ t: 'setInputText', text: e.currentTarget.value }); };
+  return <div className="interface-container">
+    <div className="textarea-container">
+      <div className="textarea-panel">
+        <div className="title">Input</div>
+        <textarea spellCheck={false} value={modeState.inputText} onInput={onInput}></textarea>
+      </div>
+      <div className="textarea-panel">
+        <div className="title">Output</div>
+        <textarea spellCheck={false} value={modeState.outputText}></textarea>
+      </div>
+    </div>
+    <div className="action-bar">
+      <button onClick={(e) => { dispatch({ t: 'setOutputText', text: encodeString(modeState.inputText) }) }}>Encode</button>
+      <button onClick={(e) => { dispatch({ t: 'setOutputText', text: decodeString(modeState.inputText) }) }}>Decode</button>
+    </div>
+  </div>;
+}
+
+function renderCommunicate(state: AppState, modeState: AppModeState & { t: 'communicate' }, dispatch: Dispatch): JSX.Element {
+  const onInput: React.FormEventHandler<HTMLTextAreaElement> = (e) => { dispatch({ t: 'setInputText', text: e.currentTarget.value }); };
+  return <div className="interface-container">
+    <div className="textarea-container">
+      <div className="textarea-panel">
+        <div className="title">Input</div>
+        <textarea spellCheck={false} value={state.modeState.inputText} onInput={onInput}></textarea>
+      </div>
+      <div className="textarea-panel">
+        <div className="title">Output</div>
+        <textarea spellCheck={false} value={state.modeState.outputText}></textarea>
+      </div>
+    </div>
+    <div className="action-bar">
+      <button onClick={(e) => { dispatch({ t: 'sendText', text: state.modeState.inputText }) }}>Send</button>
+    </div>
+  </div>;
+}
+
+function renderAppBody(state: AppState, dispatch: Dispatch): JSX.Element {
+  const onInput: React.FormEventHandler<HTMLTextAreaElement> = (e) => { dispatch({ t: 'setInputText', text: e.currentTarget.value }); };
   switch (state.mode) {
-    case 'evaluate': return <div className="interface-container">
-      <div className="textarea-container">
-        <div className="textarea-panel">
-          <div className="title">Input</div>
-          <textarea spellCheck={false} value={state.modeState.inputText} onInput={onInput}></textarea>
-        </div>
-        <div className="textarea-panel">
-          <div className="title">Output</div>
-          <textarea spellCheck={false} value={state.modeState.outputText}></textarea>
-        </div>
-      </div>
-      <div className="action-bar">
-        <button>Evaluate</button>
-      </div>
-    </div>;
-    case 'codec': return <div className="interface-container">
-      <div className="textarea-container">
-        <div className="textarea-panel">
-          <div className="title">Input</div>
-          <textarea spellCheck={false} value={state.modeState.inputText} onInput={onInput}></textarea>
-        </div>
-        <div className="textarea-panel">
-          <div className="title">Output</div>
-          <textarea spellCheck={false} value={state.modeState.outputText}></textarea>
-        </div>
-      </div>
-      <div className="action-bar">
-        <button onClick={(e) => { dispatch({ t: 'setOutputText', text: encodeString(state.modeState.inputText) }) }}>Encode</button>
-        <button onClick={(e) => { dispatch({ t: 'setOutputText', text: decodeString(state.modeState.inputText) }) }}>Decode</button>
-      </div>
-    </div>;
-    case 'communicate': return <div className="interface-container">
-      <div className="textarea-container">
-        <div className="textarea-panel">
-          <div className="title">Input</div>
-          <textarea spellCheck={false} value={state.modeState.inputText} onInput={onInput}></textarea>
-        </div>
-        <div className="textarea-panel">
-          <div className="title">Output</div>
-          <textarea spellCheck={false} value={state.modeState.outputText}></textarea>
-        </div>
-      </div>
-      <div className="action-bar">
-        <button onClick={(e) => { dispatch({ t: 'sendText', text: state.modeState.inputText }) }}>Send</button>
-      </div>
-    </div>;
+    case 'evaluate':
+      if (state.modeState.t != state.mode) throw new Error(`mode state inconsistency`);
+      return renderEvaluate(state, state.modeState, dispatch);
+    case 'codec':
+      if (state.modeState.t != state.mode) throw new Error(`mode state inconsistency`);
+      return renderCodec(state, state.modeState, dispatch);
+    case 'communicate':
+      if (state.modeState.t != state.mode) throw new Error(`mode state inconsistency`);
+      return renderCommunicate(state, state.modeState, dispatch);
   }
 }
 
