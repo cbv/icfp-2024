deb09cbc77c561bc99ed9f1ea4ac9584eb814bd3
David Renshaw <dwrenshaw@gmail.com>
2024-06-27 22:47:22 -0400

    example rust program using API_KEY

---
+++ rust/test-api-token.rs
@@ -0,0 +1,28 @@
+use anyhow::anyhow;
+
+const API_KEY_PATH : &str = "API_KEY";
+
+pub fn get_api_token() -> Result<String, std::io::Error> {
+    match std::fs::read_to_string(API_KEY_PATH) {
+        Ok(s) => Ok(s.trim().into()),
+        Err(e) => {
+            eprintln!(
+                "Failed to read API key. please download an API key from \
+                 https://icfpcontest2024.github.io/team.html and save it \
+                 in a file named {API_KEY_PATH}.\n");
+            Err(e)
+        }
+    }
+}
+
+fn main() -> Result<(), anyhow::Error> {
+    let token = get_api_token()?;
+    let response = ureq::get("https://boundvariable.space/team")
+        .set("Authorization", &format!("Bearer {token}"))
+        .call()?;
+    if response.status() != 200 {
+        return Err(anyhow!("non-200 response: {}", response.status()))
+    }
+    println!("{}", response.into_string()?);
+    Ok(())
+}
