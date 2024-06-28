f68055b3945054d4153597baf6fcb13dee8dc828
David Renshaw <dwrenshaw@gmail.com>
2024-06-28 11:54:43 -0400

    more lambdaman stuff

--- lambdaman-solver/src/lib.rs
+++ lambdaman-solver/src/lib.rs
@@ -8,21 +8,55 @@ pub enum Item {
     Man,
 }
 
-/// (0,0) is upper left
 #[derive(Debug, Clone, Copy, PartialEq, Hash, PartialOrd)]
-pub struct Coords {
-    row : i32,
-    col : i32,
+pub enum Move {
+    Left,
+    Right,
+    Up,
+    Down,
 }
+
+/// (0,0) is upper left
+#[derive(Debug, Clone, Copy, PartialEq, Hash, PartialOrd, Default)]
+pub struct Coords {
+    row : usize,
+    col : usize,
+}
+
+impl Coords {
+    pub fn new(row : usize, col : usize) -> Self {
+        Self { row, col }
+    }
+}
+
 
 #[derive(Debug, Clone, PartialEq, Hash, PartialOrd)]
 pub struct State {
     cells: Vec<Vec<Item>>,
+    man: Coords,
+}
+
+impl std::ops::Index<Coords> for State {
+    type Output = Item;
+
+    fn index(&self, index: Coords) -> &Item {
+        &self.cells[index.row as usize][index.col as usize]
+    }
 }
 
 impl State {
     pub fn new(cells: Vec<Vec<Item>>) -> Self {
-        Self { cells }
+        let mut man : Coords = Default::default();
+        for ridx in 0..cells.len() {
+            let row = &cells[ridx];
+            for cidx in 0..row.len() {
+                let cell = row[cidx];
+                if cell == Item::Man {
+                    man = Coords::new(ridx, cidx)
+                }
+            }
+        }
+        Self { cells, man }
     }
 }
 
