29921ba9d7240bac03bee174bb50b49fe38f409c
David Renshaw <dwrenshaw@gmail.com>
2024-06-29 16:20:15 -0400

    check the file extension

--- lambdaman-solver/src/bin/print_states.rs
+++ lambdaman-solver/src/bin/print_states.rs
@@ -1,4 +1,8 @@
+use anyhow::anyhow;
+
 use std::fs;
+//use std::process;
+use std::ffi::OsStr;
 
 pub fn main() -> anyhow::Result<()> {
     // two args: 1. problem file
@@ -9,13 +13,28 @@ pub fn main() -> anyhow::Result<()> {
         eprintln!("usage: {} PROBLEM_FILE SOLUTION_FILE", args[0]);
     }
 
-    let problem = fs::read_to_string(&args[1])?;
-    let mut state = lambdaman::parse_lambda_puzzle(problem);
+    let solution_path = std::path::Path::new(&args[2]);
     let solution = fs::read_to_string(&args[2])?;
 
-    // need to strip away the "solve lambdamanX "
-    let moves_str = solution.split_whitespace().nth(2).unwrap();
-    let moves = lambdaman::parse_moves(&moves_str)?;
+    let moves = match solution_path.extension().and_then(OsStr::to_str).unwrap() {
+        "icfp" => {
+            //process::Command::new("../cc/eval.exe");
+            // ...
+            todo!()
+        }
+        "txt" => {
+            // need to strip away the "solve lambdamanX "
+            let moves_str = solution.split_whitespace().nth(2).unwrap();
+            let moves = lambdaman::parse_moves(&moves_str)?;
+            moves
+        }
+        e => {
+            return Err(anyhow!("unknown extension: {e}"))
+        }
+    };
+
+    let problem = fs::read_to_string(&args[1])?;
+    let mut state = lambdaman::parse_lambda_puzzle(problem);
     let mut idx = 0;
     while !state.is_done() {
         println!("{state}");
