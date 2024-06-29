b87c06299473ed4df5bb954a2056ce1f41394a66
Jason Reed <jcreed@gmail.com>
2024-06-29 14:42:52 -0400

    Strip whitespace in send/send-raw

--- gui/src/send-stdin.ts
+++ gui/src/send-stdin.ts
@@ -2,7 +2,7 @@ import { simpleStringReq } from './simple-string-req';
 import * as fs from 'fs';
 
 async function go() {
-  const input = fs.readFileSync(0, 'utf8').replace(/\n$/, '');
+  const input = fs.readFileSync(0, 'utf8').replace(/\s+$/, '').replace(/^\s+/, '');
   console.log(await simpleStringReq(input));
 }
 
