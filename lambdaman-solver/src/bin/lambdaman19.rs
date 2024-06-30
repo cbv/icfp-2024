c07d91dbaf1860c949afb22d6111715466361169
David Renshaw <dwrenshaw@gmail.com>
2024-06-30 17:36:18 -0400

    lambdaman19: 5144 -> 385

--- lambdaman-solver/src/bin/lambdaman19.rs
+++ lambdaman-solver/src/bin/lambdaman19.rs
@@ -3,37 +3,50 @@ use icfp::expr::*;
 pub fn main() -> anyhow::Result<()> {
     //oblique square fractal thing
 
-    // use a linear congruential RNG and do a random walk
-    // (this doesn't actually work on this problem)
+    // f(n):
+    //  if n = 0 then return
+    //  let g s :=
+    //    walk n in direction (take 1 s)
+    //    f(n/2)
+    //    walk n in direction (drop 1 s)
+    //  concat(g("UD"),
+    //    concat(g("DU"),
+    //      concat(g("LR"), g("RL"))))
+
+
     let walk =
-        rec("S",
+        rec("f",
             lam("n",
-                lam("r",
-                    // have we finished?
-                    cond(equ(vuse("n"), litnum(1000000)),
-                         // done
-                         litstr(""),
-                         // Otherwise do some moves and recurse
-                         concat(
-                             let1("x",
-                                  modulus(div(vuse("r"), litnum(0x0fffffff)), litnum(4)),
-                                  cond(equ(litnum(0), vuse("x")),
-                                       litstr("U"),
-                                       cond(equ(litnum(1), vuse("x")),
-                                            litstr("D"),
-                                            cond(equ(litnum(2), vuse("x")),
-                                                 litstr("L"),
-                                                 litstr("R"))))),
-                             app_spine(
-                                 vuse("S"),
-                                 vec![add(vuse("n"), litnum(1)),
-                                      modulus(add(mul(vuse("r"), litnum(1664525)),
-                                                  litnum(1013904223)),
-                                              litnum(0x100000000))]))))));
+                cond(
+                    equ(vuse("n"), litnum(0)),
+                    litstr(""),
+                    let1(
+                        "g",
+                        lam("s",
+                            concat(
+                                sapp_spine(
+                                    repeat(),
+                                    vec![take(litnum(1), vuse("s")),
+                                         vuse("n")]),
+                                concat(
+                                    sapp(vuse("f"),
+                                         div(vuse("n"), litnum(2))),
+                                    sapp_spine(
+                                        repeat(),
+                                        vec![drop(litnum(1), vuse("s")),
+                                             vuse("n")])))),
+                        concat(
+                            sapp(vuse("g"), litstr("UD")),
+                            concat(
+                                sapp(vuse("g"), litstr("DU")),
+                                concat(
+                                    sapp(vuse("g"), litstr("LR")),
+                                    sapp(vuse("g"), litstr("RL")))))))));
+
 
     let e = concat(litstr("solve lambdaman19 "),
-                   app_spine(walk,
-                             vec![litnum(0), litnum(42)]));
+                   sapp(walk, litnum(64)));
+
     println!("{e}");
 
     Ok(())
