af2f99c2319b9a35c447e84b82b037d55f97ed0a
Jim McCann <ix@tchow.com>
2024-07-01 04:42:19 -0400

    launchpad-y version also too slow

--- seaplusplus/make-3d11.js
+++ seaplusplus/make-3d11.js
@@ -12,6 +12,9 @@ Make this sort of read-back structure:
  . . . . . . . S . .
 */
 
+// What if read-back looked more like this:
+
+
 const grid = [];
 
 let columns = 0;
@@ -25,22 +28,81 @@ function write(gx, gy, c) {
 
 const SIZE = 101;
 
+function extract(str) {
+	const got = [];
+	for (const line of str.split(/\s*[\r\n]+\s*/)) {
+		const row = [];
+		for (let col of line.split(/\s+/)) {
+			row.push(col);
+		}
+		got.push(row);
+	}
+	return got;
+}
+
+const TILE = extract(
+`.  .  .  .  .  @  .  .  .  .
+ .  .  .  .  .  #  .  >  .  .
+ .  .  .  1  ^  .  .  5  @ -1
+ .  .  S  @  .  .  .  .  4  .
+ .  .  .  .  .  0  .  .  .  .
+ .  .  1  *  .  *  .  >  .  .
+ .  .  .  .  .  .  .  6  @  3
+ .  .  .  v  .  . YA  .  4  .
+ . XA  .  .  .  YB *  .  .  .
+XB  *  .  v  .  .  .  .  .  .
+ .  .  .  .  .  YC +  .  .  .
+XC  +  .  @  .  <  .  .  .  .
+ .  .  .  4  .  .  .  .  .  .`
+);
+
+//console.log(TILE);
+//process.exit(1);
+
+const TW = TILE[0].length;
+const TH = TILE.length;
+
+const TARGET_X = 21;
+const TARGET_Y = 6;
+
+const SOURCE_X = 19;
+const SOURCE_Y = 4;
+
+const ORIGIN_X = TW * 1; //these need to be the same multiple because the '*' in the corner write the same tile :-(
+const ORIGIN_Y = TH * 1;
+
+console.assert(ORIGIN_X % TW == 0, "Origin must be multiple of width.");
+console.assert(ORIGIN_Y % TH == 0, "Origin must be multiple of height.");
+
 for (let y = 0; y < SIZE; ++y) {
 	for (let x = 0; x < SIZE; ++x) {
-		let gx = (x + 10) * 2 + 19;
-		let gy = (y + 10) * 2 + 4;
-		write(gx, gy, (x == 0 && y == 0 ? '1' : '0'));
-		write(gx, gy+1, '+');
-		if (x == 0) {
-			write(gx-1,gy+1,'0');
-		}
-		if (x + 1 == SIZE) {
-			write(gx+2, gy+1, '+');
-			if (y == 0) {
-				write(gx+2, gy, '0');
-			}
-			if (y + 1 == SIZE) {
-				write(gx+2, gy+2, 'S');
+		const gx = x * TW + ORIGIN_X + SOURCE_X - 3;
+		const gy = y * TH + ORIGIN_Y + SOURCE_Y - 3;
+
+		const dx = gx + 4 - TARGET_X;
+		const dy = gy + 11 - TARGET_Y;
+
+		const subst = { };
+		subst.XA = Math.floor(dx/99);
+		subst.XB = 99;
+		subst.XC = dx - (subst.XA * subst.XB);
+
+		subst.YA = Math.floor(dy/99);
+		subst.YB = 99;
+		subst.YC = dy - (subst.YA * subst.YB);
+		
+		subst.DX = (subst.XA * subst.XB) + subst.XC;
+		subst.DY = (subst.YA * subst.YB) + subst.YC;
+
+		subst.S = (x == 0 && y == 0 ? '0' : '1');
+
+		for (let ty = 0; ty < TILE.length; ++ty) {
+			for (let tx = 0; tx < TILE[ty].length; ++tx) {
+				let cell = TILE[ty][tx];
+				if (cell in subst) {
+					cell = subst[cell];
+				}
+				write(gx + tx, gy + ty, cell);
 			}
 		}
 	}
@@ -48,7 +110,7 @@ for (let y = 0; y < SIZE; ++y) {
 
 const fs = require('fs');
 
-const corner = fs.readFileSync('../solutions/threed/threed11-corner.txt', 'utf8');
+const corner = fs.readFileSync('../solutions/threed/threed11-corner.txt', 'utf8').trim();
 {
 	let y = 0;
 	for (const line of corner.split(/\s*[\r\n]+\s*/).slice(1)) {
@@ -58,6 +120,10 @@ const corner = fs.readFileSync('../solutions/threed/threed11-corner.txt', 'utf8'
 			if (col == 'DX') col = `${SIZE - 3}`;
 			if (col == 'S1') col = `${Math.floor(SIZE/2)}`;
 			if (col == 'S2') col = `${SIZE - Math.floor(SIZE/2)}`;
+			if (col == 'OX') col = `${ORIGIN_X / TW}`;
+			if (col == 'OY') col = `${ORIGIN_Y / TH}`;
+			if (col == 'SX') col = `${-TW}`;
+			if (col == 'SY') col = `${-TH}`;
 			write(x,y,col);
 			x += 1;
 		}
@@ -75,10 +141,13 @@ for (let row = 0; row < grid.length; ++row) {
 	out += '\n';
 	for (let col = 0; col < grid[row].length; ++col) {
 		let str = grid[row][col];
+		out += ' ' + str;
+		/*
 		if (str.length == 1) str = ' ' + str + ' ';
 		else if (str.length == 2) str = str + ' ';
-		else /* hope */;
+		else str = str + ' '; //gotta pad them somehow
 		out += str;
+		*/
 	}
 }
 console.log(out);
